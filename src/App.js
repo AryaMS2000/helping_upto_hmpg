a5f3b397b5f42a5a625d8248ddc327113c2969c4
AryaMS2000 <78840262+AryaMS2000@users.noreply.github.com>
2021-08-05 21:31:57 +0530

    New pages route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Register from './Components/Register';
@@ -8,6 +7,12 @@ import home from './Components/home';
 import Homebar from './Components/Homebar';
 import apphome from './Components/apphome';
 import Adminpg from './Components/Adminmainpg';
+import Profilepg from './Components/Profile';
+import Dashboardpg from './Components/Dashboard';
+import Weekend from './Components/Weekend';
+import Reguser from './Components/Registereduser';
+import Past from './Components/PastEvents';
+import Events from './Components/EventsReg';
 
 function App() {
   return (
@@ -19,6 +24,12 @@ function App() {
     <Route exact path='/Homebar'  component={Homebar}/>
     <Route exact path='/apphome'  component={apphome}/>
     <Route exact path='/Adminmainpg'  component={Adminpg}/>
+    <Route exact path='/Profile'  component={Profilepg}/>
+    <Route exact path='/Dashboard'  component={Dashboardpg}/>
+    <Route exact path='/Weekend'  component={Weekend}/>
+    <Route exact path='/Registereduser'  component={Reguser}/>
+    <Route exact path='/PastEvents'  component={Past}/>
+    <Route exact path='/EventsRegistered'  component={Events}/>
    </Switch>
    </Router>
      )
